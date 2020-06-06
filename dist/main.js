6356b34d2d2867ed8cdc9e484d5a4fce48f6619f
Gustavo Collaço <gustavo.m.collaco@gmail.com>
2020-06-06 19:20:29 -0300

    react now a peer dependency; eulexia component listening to esq key

--- dist/main.js
+++ dist/main.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import clsx from 'clsx';
 
 function _extends() {
@@ -55,6 +55,62 @@ function _objectWithoutProperties(source, excluded) {
   return target;
 }
 
+function _slicedToArray(arr, i) {
+  return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest();
+}
+
+function _arrayWithHoles(arr) {
+  if (Array.isArray(arr)) return arr;
+}
+
+function _iterableToArrayLimit(arr, i) {
+  if (typeof Symbol === "undefined" || !(Symbol.iterator in Object(arr))) return;
+  var _arr = [];
+  var _n = true;
+  var _d = false;
+  var _e = undefined;
+
+  try {
+    for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) {
+      _arr.push(_s.value);
+
+      if (i && _arr.length === i) break;
+    }
+  } catch (err) {
+    _d = true;
+    _e = err;
+  } finally {
+    try {
+      if (!_n && _i["return"] != null) _i["return"]();
+    } finally {
+      if (_d) throw _e;
+    }
+  }
+
+  return _arr;
+}
+
+function _unsupportedIterableToArray(o, minLen) {
+  if (!o) return;
+  if (typeof o === "string") return _arrayLikeToArray(o, minLen);
+  var n = Object.prototype.toString.call(o).slice(8, -1);
+  if (n === "Object" && o.constructor) n = o.constructor.name;
+  if (n === "Map" || n === "Set") return Array.from(o);
+  if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen);
+}
+
+function _arrayLikeToArray(arr, len) {
+  if (len == null || len > arr.length) len = arr.length;
+
+  for (var i = 0, arr2 = new Array(len); i < len; i++) arr2[i] = arr[i];
+
+  return arr2;
+}
+
+function _nonIterableRest() {
+  throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.");
+}
+
 var Button = function Button(_ref) {
   var className = _ref.className,
       props = _objectWithoutProperties(_ref, ["className"]);
@@ -68,9 +124,32 @@ var Eulexia = function Eulexia(_ref) {
   var className = _ref.className,
       props = _objectWithoutProperties(_ref, ["className"]);
 
-  return /*#__PURE__*/React.createElement("button", _extends({
-    className: clsx(className, 'lexia')
-  }, props));
+  var _useState = useState(0),
+      _useState2 = _slicedToArray(_useState, 2),
+      mode = _useState2[0],
+      setMode = _useState2[1];
+
+  var _useState3 = useState(false),
+      _useState4 = _slicedToArray(_useState3, 2),
+      visible = _useState4[0],
+      setVisible = _useState4[1];
+
+  var escFunction = useCallback(function (event) {
+    if (event.keyCode === 27) {
+      setVisible(function (prev) {
+        return !prev;
+      });
+    }
+  });
+  useEffect(function () {
+    document.addEventListener("keydown", escFunction, false);
+    return function () {
+      document.removeEventListener("keydown", escFunction, false);
+    };
+  });
+  return /*#__PURE__*/React.createElement("input", {
+    value: visible
+  });
 };
 
 var Input = function Input(_ref) {
--- src/eulexia/index.js
+++ src/eulexia/index.js
@@ -1,12 +1,25 @@
-import React from 'react'
-import clsx from 'clsx'
+import React, { useCallback, useEffect, useState } from 'react'
 
 const Eulexia = ({className, ...props}) => {
+    const [mode, setMode] = useState(0)
+    const [visible, setVisible] = useState(false)
+
+    const escFunction = useCallback((event) => {
+        if(event.keyCode === 27) {
+            setVisible((prev) => !prev)
+        }
+    })
+
+    useEffect(() => {
+        document.addEventListener("keydown", escFunction, false);
+
+        return () => {
+            document.removeEventListener("keydown", escFunction, false);
+        }
+    })
+    
     return (
-        <button
-            className={clsx(className, 'lexia')}
-            {...props}
-            />
+        <input value={visible}></input>
     )
 }
 
