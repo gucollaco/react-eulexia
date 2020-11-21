007b47719747e949cf33bcc7acec3728a5df8e6a
Gustavo Collaço <gustavo.m.collaco@gmail.com>
2020-11-21 09:54:12 -0300

    fix: remove unexistent component from export

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import Button from './button/index.jsx'
-import Eulexia from './eulexia/index.jsx'
 import Input from './input/index.jsx'
 import EulexiaFab from './eulexiaFab/index.jsx'
 import EulexiaAction from './eulexiaAction/index.jsx'
 
-export { Button, Eulexia, Input, EulexiaFab, EulexiaAction }
+export { Button, Input, EulexiaFab, EulexiaAction }
