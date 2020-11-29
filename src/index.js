d16cbbcf85d7f576d7bcdcb7fc60a2339522d27d
Gustavo Collaço <gustavo.m.collaco@gmail.com>
2020-11-29 02:55:25 -0300

    refactor: update play and stop button code and appearance

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-import Button from './button/index.jsx'
 import Input from './input/index.jsx'
 import EulexiaFab from './eulexiaFab/index.jsx'
 import EulexiaAction from './eulexiaAction/index.jsx'
 
-export { Button, Input, EulexiaFab, EulexiaAction }
+export { Input, EulexiaFab, EulexiaAction }
