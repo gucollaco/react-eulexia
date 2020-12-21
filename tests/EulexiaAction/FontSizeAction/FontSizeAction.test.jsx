38e492287124012f1eca886f34ae88b13d8fe2ad
Gustavo Collaço <gustavo.m.collaco@gmail.com>
2020-12-20 22:08:08 -0300

    refactor: font size action test rendering and resetting component on beforeEach

--- tests/EulexiaAction/FontSizeAction/FontSizeAction.test.jsx
+++ tests/EulexiaAction/FontSizeAction/FontSizeAction.test.jsx
@@ -5,26 +5,28 @@ import '@testing-library/jest-dom'
 import EulexiaFab from 'EulexiaFab/EulexiaFab.jsx'
 import FontSizeAction from 'EulexiaAction/FontSizeAction/FontSizeAction.jsx'
 
+const resetFontSizeAction = () => {
+  const fontSizeToggle = screen.getByTestId('font-size-toggle')
+  if (fontSizeToggle.checked) fireEvent.click(fontSizeToggle)
+}
+
 describe('FontSizeAction', () => {
-  test('Checking the texts shown on the Action', () => {
+  beforeEach(() => {
     render(
       <EulexiaFab>
         <FontSizeAction />
       </EulexiaFab>
     )
+    resetFontSizeAction()
+  })
 
+  test('Checking the texts shown on the Action', () => {
     expect(screen.getByText('Font size')).toBeInTheDocument()
     expect(screen.getByText('Headers')).toBeInTheDocument()
     expect(screen.getByText('Texts')).toBeInTheDocument()
   })
 
   test('Trying to edit slider value before activating toggle', () => {
-    render(
-      <EulexiaFab>
-        <FontSizeAction />
-      </EulexiaFab>
-    )
-
     const headerFontSizeSlider = screen.getByTestId('header-font-size-slider')
     expect(headerFontSizeSlider).toHaveAttribute('value', '0')
     headerFontSizeSlider.firstChild.focus()
@@ -36,12 +38,6 @@ describe('FontSizeAction', () => {
   })
 
   test('Switching toggle button to on, changing the text size values and switching back off', () => {
-    render(
-      <EulexiaFab>
-        <FontSizeAction />
-      </EulexiaFab>
-    )
-
     const fontSizeToggle = screen.getByTestId('font-size-toggle')
     expect(fontSizeToggle.checked).toEqual(false)
     fireEvent.click(fontSizeToggle)
