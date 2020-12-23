81c44608fbdee7b02bd3913737d0e61b8a0e9276
Gustavo Collaço <gustavo.m.collaco@gmail.com>
2020-12-23 03:08:28 -0300

    test(config): coverage threshold values set

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,13 @@ module.exports = {
   moduleDirectories: ['node_modules', 'src'],
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.{js,jsx}'],
-  setupFiles: ['<rootDir>/.jest/env.js']
+  setupFiles: ['<rootDir>/.jest/env.js'],
+  coverageThreshold: {
+    global: {
+      branches: 75,
+      functions: 75,
+      lines: 75,
+      statements: 75
+    }
+  }
 }
