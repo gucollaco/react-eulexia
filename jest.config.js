8dd214ad24c2fa061c43fecfbf0e7d63f99784e7
Gustavo Collaço <gustavo.m.collaco@gmail.com>
2020-12-26 16:23:46 -0300

    test: test ci failure due to jest error

--- jest.config.js
+++ jest.config.js
@@ -6,10 +6,10 @@ module.exports = {
   setupFiles: ['<rootDir>/.jest/env.js'],
   coverageThreshold: {
     global: {
-      branches: 75,
-      functions: 75,
-      lines: 75,
-      statements: 75
+      branches: 95,
+      functions: 95,
+      lines: 95,
+      statements: 95
     }
   }
 }
