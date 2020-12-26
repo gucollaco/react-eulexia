7f3acdcd06f46c78dc2918841c3bc3553d37f890
Gustavo Collaço <gustavo.m.collaco@gmail.com>
2020-12-26 16:17:50 -0300

    feat(config): codecov setup with circle ci

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   moduleDirectories: ['node_modules', 'src'],
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.{js,jsx}'],
+  coverageDirectory: './coverage/',
   setupFiles: ['<rootDir>/.jest/env.js'],
   coverageThreshold: {
     global: {
