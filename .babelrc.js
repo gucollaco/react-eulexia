6da7b6b22f1c74a861572bef66334008b11af9d7
Gustavo Collaço <gustavo.m.collaco@gmail.com>
2020-12-27 12:37:01 -0300

    chore: update rollup to runtime

--- .babelrc.js
+++ .babelrc.js
@@ -1,4 +1,7 @@
 module.exports = {
   presets: ["@babel/env", "@babel/react"],
-  plugins: ["babel-plugin-styled-components"],
+  plugins: [
+    "babel-plugin-styled-components",
+    "@babel/plugin-transform-runtime",
+  ],
 };
